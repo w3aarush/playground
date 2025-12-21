70ddd9ac015be286e28bd400f521ffe0fcefd2a1
w3aarush <w3aarush@gmail.com>
2025-12-21 23:42:58 +0530

    tried to make a chatbot with langchain using ollama downloaded model

---
+++ langchain/chatbot.py
@@ -0,0 +1,18 @@
+from langchain_classic.chains import ConversationChain
+from langchain_classic.memory import ConversationBufferMemory
+from langchain_community.chat_models import ChatOllama
+
+chat = ChatOllama(model="qwen2.5-coder:1.5b", temperature=0.7)
+memory = ConversationBufferMemory()
+conversation = ConversationChain(llm=chat, memory=memory, verbose=False)
+
+print("Local LangChain Chatbot (type 'exit' to quit)\n")
+
+while True:
+    user_input = input("You: ")
+
+    if user_input.lower() in ["exit", "quit", "bye"]:
+        print("Bot: Bye!")
+
+    response = conversation.predict(input=user_input)
+    print(f"Bot: {response}")
