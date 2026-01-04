117a56c37a16d643004fc19d4f08df77e5192a92
w3aarush <w3aarush@gmail.com>
2026-01-05 00:27:12 +0530

    succesfully implemented vector database

--- chatbot_semantic/semantic_chatbot2/semantic_chatbot2.py
+++ chatbot_semantic/semantic_chatbot2/semantic_chatbot2.py
@@ -1,7 +1,23 @@
 import chromadb as db
 import pandas as pd
 
-# loading data from csv file
-chat_data = pd.read_csv('./chatdata.csv')
+client = db.PersistentClient('./chat_database')
+collection = client.get_collection('chatdata')
+print('collection loaded succesfully.')
 
-persistent_client = chromadb.Persistent
+THRESHOLD = 0.25
+
+def chatbot():
+    while True:
+        message = input("User: -> \n")
+        if message == 'exit':
+            break
+        score = collection.query(query_texts=[message],n_results=1)['distances'][0][0]
+        if score<=THRESHOLD:
+            res = collection.query(query_texts=[message],n_results=1)['metadatas'][0][0]['response']
+            print(f"Bot: -> {res}")
+        else:
+            res = collection.query(query_texts=[message],n_results=4)['metadatas'],collection.query(query_texts=[message],n_results=4)['distances']
+            print(f"Bot:-. {res}")
+
+chatbot()
